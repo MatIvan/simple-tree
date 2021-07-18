c301a66cf1318060f5c03b376fcdd40c45335256
mativan <mativ.rufous@yandex.ru>
2021-07-18 08:48:03 +0300

    узлы теперь можно раскрывать/закрывать

--- client-react/src/components/NodeItem.js
+++ client-react/src/components/NodeItem.js
@@ -10,10 +10,34 @@ class NodeItem extends Component {
             isLoaded: true,
             error: null,
             nodes: [],
+            isCollapsed: true,
         };
     }
 
     componentDidMount() {
+        //do nothing
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.isCollapsed !== this.state.isCollapsed) {
+            if (this.state.isCollapsed) {
+                this.setState({
+                    isLoaded: true,
+                    error: null,
+                    nodes: [],
+                });
+            } else {
+                this.setState({
+                    isLoaded: false,
+                    error: null,
+                    nodes: [],
+                });
+                this.sendRequest();
+            }
+        }
+    }
+
+    sendRequest() {
         TreeNodeService.getChildren(this.props.node.id)
             .then(result => {
                 this.setState({
@@ -31,6 +55,14 @@ class NodeItem extends Component {
             });
     }
 
+    onNodeItemClick() {
+        const prev = this.state.isCollapsed;
+        this.setState({
+            isCollapsed: !prev,
+        });
+        console.log(this.state.isCollapsed);
+    }
+
     render() {
         const { error, isLoaded, nodes } = this.state;
         let data;
@@ -46,10 +78,12 @@ class NodeItem extends Component {
         }
 
         const nodeInfo = this.props.node;
+        const buttonText = this.state.isCollapsed ? "+" : "-";
         return (
             <div className={styles.box}>
-                <div>
-                    <div>{nodeInfo.name}</div>
+                <div className={styles.infoBox} onClick={() => this.onNodeItemClick()}>
+                    <div className={styles.btnCollapse}>{buttonText}</div>
+                    <div className={styles.lblName}>{nodeInfo.name}</div>
                 </div>
                 {data}
             </div>
